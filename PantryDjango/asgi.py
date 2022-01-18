ccabe0410e32f1b7b90e59734bf02ff0d7fd47af
Ben Brenner <ben.brenner@gmail.com>
2022-01-17 20:56:48 -0600

    change name to pantrydjango

--- PantryDjango/asgi.py
+++ PantryDjango/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for PantryDjango project.
+ASGI config for pantrydjango project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PantryDjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pantrydjango.settings')
 
 application = get_asgi_application()
--- PantryDjango/wsgi.py
+++ PantryDjango/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PantryDjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pantrydjango.settings')
 
 application = get_wsgi_application()
